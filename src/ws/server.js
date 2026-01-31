942dced147bbf434a065c772561f530090a6722c
Satyam Raj <satyamrajgaya434@gmail.com>
2026-01-31 18:24:25 +0530

    Implement Bug Fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -22,6 +22,35 @@ export function attachWebSocketServer(server) {
     maxPayload: 1024 * 1024, // 1 MB
   });
 
+  server.on("upgrade", async (req, socket, head) => {
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+    if (pathname === "/ws") {
+      return;
+    }
+
+    if (wsArcjet) {
+      try {
+        const decision = await wsArcjet.protect(req);
+
+        if (decision.isDenied()) {
+          if (decision.reason.isRateLimit()) {
+            socket.write("HTTP/1.1 429 Too Many Requests\r\n\r\n");
+          } else {
+            socket.write("HTTP/1.1 403 Forbidden\r\n\r\n");
+          }
+          socket.destroy();
+          return;
+        }
+      } catch (e) {
+        console.error("WS Upgrade protection error", e);
+        socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        socket.destroy();
+        return;
+      }
+    }
+  });
+
   wss.on("connection", async (socket, req) => {
     if (wsArcjet) {
       try {
